23cd645578d9688ddb798aa7738f89e796feff7b
Simon Lambin <slambin@artfx.fr>
2021-04-15 11:43:26 +0200

    feat(TerrainErosion scene): Add compute shader

--- AdvancedOpenGL/src/game/Scene_024_TerrainErosion.cpp
+++ AdvancedOpenGL/src/game/Scene_024_TerrainErosion.cpp
@@ -5,6 +5,7 @@
 #include "Scene_024_TerrainErosion.h"
 #include "../engine/Timer.h"
 #include "../engine/MacroUtils.h"
+#include "../engine/Log.h"
 
 #include <cstdlib>
 #include <ctime>
@@ -57,6 +58,10 @@ void Scene_024_TerrainErosion::handleEvent(const InputState &inputState) {
 }
 
 void Scene_024_TerrainErosion::load() {
+    ////////////////////////////////////////
+    // Graphic shader
+    ////////////////////////////////////////
+    
     Assets::loadShader(SHADER_VERT(SHADER_NAME), SHADER_FRAG(SHADER_NAME), SHADER_TECS(SHADER_NAME), SHADER_TESE(SHADER_NAME), "", SHADER_ID(SHADER_NAME));
 
     // Create the vertex array that will store the geometry and bind it
@@ -79,6 +84,37 @@ void Scene_024_TerrainErosion::load() {
     glBindTexture(GL_TEXTURE_2D, texColor);
 
     shader = Assets::getShader(SHADER_ID(SHADER_NAME));
+
+
+    ////////////////////////////////////////
+    // Compute shader
+    ////////////////////////////////////////
+
+    Assets::loadComputeShader(SHADER_COMP(SHADER_NAME), SHADER_ID(SHADER_NAME));
+
+    // Create two buffers that will hold the data of the input and the output of the compute shader
+    glGenBuffers(2, dataBuffer);
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, dataBuffer[0]);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, NUM_ELEMENTS * sizeof(float), NULL, GL_DYNAMIC_DRAW);
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, dataBuffer[1]);
+    glBufferData(GL_SHADER_STORAGE_BUFFER, NUM_ELEMENTS * sizeof(float), NULL, GL_DYNAMIC_COPY);
+
+    // Initialize the input buffer with random values
+    for (int i = 0; i < NUM_ELEMENTS; i++)
+    {
+        inputData[i] = randomFloat();
+    }
+
+    // Bind the program of the shader
+    cShader = Assets::getComputeShader(SHADER_ID(SHADER_NAME));
+
+    // Set the binding of the storage buffer on the shader
+    // Here, we attach the buffer to the binding 0 of the shader
+    // And the buffer 1 to the binding 1 of the shader
+    glShaderStorageBlockBinding(cShader.id, 0, 0);
+    glShaderStorageBlockBinding(cShader.id, 1, 1);
 }
 
 void Scene_024_TerrainErosion::update(float dt) {
@@ -89,6 +125,46 @@ void Scene_024_TerrainErosion::update(float dt) {
     t = totalTime * 0.03f;
     r = sinf(t * 5.37f) * 15.0f + 16.0f;
     h = cosf(t * 4.79f) * 2.0f + 10.2f;
+
+
+    ////////////////////////////////////////
+    // Compute shader
+    ////////////////////////////////////////
+    
+    // Initialize the pointer to the output data
+    float* ptr;
+
+    // Initialize and pass the input array to the buffer 0
+    glBindBufferRange(GL_SHADER_STORAGE_BUFFER, 0, dataBuffer[0], 0, sizeof(float) * NUM_ELEMENTS);
+    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, sizeof(float) * NUM_ELEMENTS, inputData);
+
+    // Initialize the buffer 1
+    glBindBufferRange(GL_SHADER_STORAGE_BUFFER, 1, dataBuffer[1], 0, sizeof(float) * NUM_ELEMENTS);
+
+    // Bind the program of the shader and fire the computation
+    cShader.use();
+    // Dispach the computation on a single global working group
+    glDispatchCompute(1, 1, 1);
+
+    // Block the thread until the computation is completed
+    glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+    glFinish();
+
+    // Get the result of the buffer 1
+    glBindBufferRange(GL_SHADER_STORAGE_BUFFER, 0, dataBuffer[1], 0, sizeof(float) * NUM_ELEMENTS);
+    ptr = (float*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, sizeof(float) * NUM_ELEMENTS, GL_MAP_READ_BIT);
+
+    // Copy the buffer 1 content into an other buffer and print it
+    char buffer[1024];
+    sprintf(buffer, "SUM: %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f "
+        "%2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f %2.2f",
+        ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7],
+        ptr[8], ptr[9], ptr[10], ptr[11], ptr[12], ptr[13], ptr[14], ptr[15]);
+
+    LOG(Info) << buffer;
+
+    // Unmap the buffer (mapping a buffer means link its gpu data to cpu data so we can access it in C++)
+    glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 }
 
 void Scene_024_TerrainErosion::draw()
